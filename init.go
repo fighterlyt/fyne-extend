3f5ba345df5f0cf81ba63a7bfee68b8f73a6a825
everest <everestbangkok1@gmail.com>
2024-10-01 02:04:10 +0800

    init

--- init.go
+++ init.go
@@ -28,28 +28,28 @@ func ensureFont() error {
 	return nil
 }
 
-func Init(botToken string, to int64) error {
-	bot, err := telebot.NewBot(telebot.Settings{
+func Init(botToken string, to int64) (bot *telebot.Bot, err error) {
+	bot, err = telebot.NewBot(telebot.Settings{
 		Token: botToken,
 	})
 
 	if err != nil {
-		return errors.Wrap(err, `构建tg`)
+		return nil, errors.Wrap(err, `构建tg`)
 	}
 
 	if _, err = bot.Send(telebot.ChatID(to), `启动`); err != nil {
-		return errors.Wrap(err, `发送`)
+		return nil, errors.Wrap(err, `发送`)
 	}
 
 	if err = ensureFont(); err != nil {
-		return errors.Wrap(err, `设置字体`)
+		return nil, errors.Wrap(err, `设置字体`)
 	}
 
 	_ = os.Setenv("FYNE_FONT", finalFontPath) // 设置环境变量
 
 	if err = ensureConfig(); err != nil {
-		return errors.Wrap(err, `确认配置`)
+		return nil, errors.Wrap(err, `确认配置`)
 	}
 
-	return nil
+	return bot, nil
 }
