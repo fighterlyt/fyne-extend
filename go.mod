454e9685d509ebeab5faa045c7ce5c7b20f77f50
everest <everestbangkok1@gmail.com>
2024-10-02 14:57:37 +0800

    添加数据库

--- go.mod
+++ go.mod
@@ -9,11 +9,19 @@ require (
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/ncruces/go-sqlite3 v0.18.4 // indirect
+	github.com/ncruces/go-sqlite3/gormlite v0.18.4 // indirect
+	github.com/ncruces/julianday v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/tetratelabs/wazero v1.8.0 // indirect
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/telebot.v3 v3.3.8 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
--- init.go
+++ init.go
@@ -6,8 +6,10 @@ import (
 	"path/filepath"
 	"runtime"
 
+	"github.com/ncruces/go-sqlite3/gormlite"
 	"github.com/pkg/errors"
 	"gopkg.in/telebot.v3"
+	"gorm.io/gorm"
 )
 
 var (
@@ -36,13 +38,13 @@ func ensureFont() error {
 	return nil
 }
 
-func Init(botToken string, to int64, needSend bool) (bot *telebot.Bot, err error) {
+func Init(botToken string, to int64, needSend bool, dbName string) (bot *telebot.Bot, db *gorm.DB, err error) {
 	bot, err = telebot.NewBot(telebot.Settings{
 		Token: botToken,
 	})
 
 	if err != nil {
-		return nil, errors.Wrap(err, `构建tg`)
+		return nil, nil, errors.Wrap(err, `构建tg`)
 	}
 
 	send := func(data string, needSend bool) {
@@ -52,7 +54,7 @@ func Init(botToken string, to int64, needSend bool) (bot *telebot.Bot, err error
 	}
 
 	if _, err = bot.Send(telebot.ChatID(to), `启动`); err != nil {
-		return nil, errors.Wrap(err, `发送`)
+		return nil, nil, errors.Wrap(err, `发送`)
 	}
 
 	defer func() {
@@ -64,7 +66,7 @@ func Init(botToken string, to int64, needSend bool) (bot *telebot.Bot, err error
 	send(`ensureFont`, needSend)
 
 	if err = ensureFont(); err != nil {
-		return bot, errors.Wrap(err, `设置字体`)
+		return bot, nil, errors.Wrap(err, `设置字体`)
 	}
 
 	send(`ensureFont`, needSend)
@@ -72,10 +74,22 @@ func Init(botToken string, to int64, needSend bool) (bot *telebot.Bot, err error
 	_ = os.Setenv("FYNE_FONT", finalFontPath) // 设置环境变量
 
 	if err = ensureConfig(); err != nil {
-		return bot, errors.Wrap(err, `确认配置`)
+		return bot, nil, errors.Wrap(err, `确认配置`)
 	}
 
 	send(`ensureConfig`, needSend)
 
-	return bot, nil
+	if dbName != `` {
+		if db, err = initDB(dbName); err != nil {
+			return bot, nil, errors.Wrap(err, `init DB`)
+		}
+	}
+
+	return bot, db, nil
+}
+
+func initDB(name string) (db *gorm.DB, err error) {
+	db, err = gorm.Open(gormlite.Open(filepath.Join(GetBasePath(), name+`.db`)), &gorm.Config{})
+
+	return db, err
 }
